bd62c5650bacd7b6e563e452d9bbcb979b878661
hieunv-sbt <hieunv@sabitech.vn>
2025-06-19 09:47:58 +0700

    Init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/about', function (req, res) {
+  let hello = "hello";
+  console.log(hello)
+  
+  res.send('Hello World 999');
+});
+
+app.listen(3003);
