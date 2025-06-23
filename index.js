c2e2a739b96aeddc615b29915759e911632a74a5
Hieunvph14434 <101816215+Hieunvph14434@users.noreply.github.com>
2025-06-23 10:38:11 +0700

    Update index.js sasa

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/about', function (req, res) {
-  let hello = "hello11";
-  let world = "world2e82";
+  let hello = "hello1s1";
+  let world = "world2e82s";
   console.log(hello, world);
   
   res.send('Hello World 999');
